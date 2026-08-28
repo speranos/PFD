939c433d386a30d13fc92d1516ab14885cd296bb
speranos <aoueldma@student.1337.ma>
2026-08-28 16:01:49 +0100

    05

--- Array/ex05/pimp_image.py
+++ Array/ex05/pimp_image.py
@@ -1,16 +1,7 @@
-# def ft_invert(array) -> array:
-#your code here
-
-# def ft_green(array) -> array:
-#your code here
-# def ft_blue(array) -> array:
-#your code here
-# def ft_grey(array) -> array:
-#your code 
-
 from array import array
 from load_image import ft_load
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def ft_green(array) -> array:
@@ -44,6 +35,19 @@ def ft_blue(array) -> array:
     cp = array.copy()
     cp[:, :, 0] = 0  # Set red channel to 0
     cp[:, :, 1] = 0  # Set green channel to 0
+    print("New shape:", cp.shape)
+    print(cp)
+    plt.imshow(cp)
+    plt.show()
+    return cp
+
+
+def ft_invert(array) -> array:
+    """
+    Invert the colors of the image.
+    """
+    cp = array.copy()
+    cp = 255 - cp  # Invert colors
     plt.imshow(cp)
     plt.show()
     return cp
@@ -53,16 +57,21 @@ def ft_grey(array) -> array:
     """
     Convert the image to greyscale.
     """
-
+    cp = array.copy()
+    t = cp.sum(axis=2)  # Sum of RGB channels
+    cp = t // 3
+    plt.imshow(cp, cmap='gray')
+    plt.show()
+    return cp
 
 
 def main():
     pic = ft_load("landscape.jpg")
-    # ft_red(pic)
-    # ft_green(pic)
+    ft_red(pic)
+    ft_green(pic)
     ft_blue(pic)
-    # ft_invert(pic)
-    # ft_grey(pic)
+    ft_invert(pic)
+    ft_grey(pic)
 
 
 if __name__ == "__main__":
