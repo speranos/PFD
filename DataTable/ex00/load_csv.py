f40df8f79aa5b938a0676961e2c0ee5d0259cf34
speranos <aoueldma@student.1337.ma>
2026-09-01 15:40:08 +0100

    use dataframe

--- DataTable/ex00/load_csv.py
+++ DataTable/ex00/load_csv.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-def load(path: str) -> np.ndarray:
+def load(path: str) -> pd.DataFrame | None:
     """
     Load a CSV file into a numpy array.
 
@@ -10,10 +10,11 @@ def load(path: str) -> np.ndarray:
         path (str): The path to the CSV file.
 
     Returns:
-        np.ndarray: A numpy array containing the data from the CSV file.
+        A pandas DataFrame containing the data from the CSV file,
+        or None if the file is not found.
     """
     try:
-        df: np.ndarray = pd.read_csv(path).to_numpy()
+        df = pd.read_csv(path)
         print("Loading dataset of dimensions ", df.shape)
         return df
     except FileNotFoundError:
@@ -26,11 +27,7 @@ def main():
     Main function to load a CSV file and print its contents.
     """
     path = "life_expectancy_years.csv"
-    data = load(path)
-    if data is not None:
-        print("Data: ", data)
-    else:
-        print("Failed to load data.")
+    print(load(path))
 
 
 if __name__ == "__main__":
--- DataTable/ex01/load_csv.py
+++ DataTable/ex01/load_csv.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-def load(path: str) -> np.ndarray:
+def load(path: str) -> pd.DataFrame | None:
     """
     Load a CSV file into a numpy array.
 
@@ -10,28 +10,13 @@ def load(path: str) -> np.ndarray:
         path (str): The path to the CSV file.
 
     Returns:
-        np.ndarray: A numpy array containing the data from the CSV file.
+        A pandas DataFrame containing the data from the CSV file,
+        or None if the file is not found.
     """
     try:
-        df: np.ndarray = pd.read_csv(path).to_numpy()
+        df = pd.read_csv(path)
         print("Loading dataset of dimensions ", df.shape)
         return df
     except FileNotFoundError:
         print("File not found.")
         return None
-
-
-def main():
-    """
-    Main function to load a CSV file and print its contents.
-    """
-    path = "life_expectancy_years.csv"
-    data = load(path)
-    if data is not None:
-        print("Data: ", data)
-    else:
-        print("Failed to load data.")
-
-
-if __name__ == "__main__":
-    main()
