50a14ee12b5bddde9e544446993d36059259f4fe
speranos <aoueldma@student.1337.ma>
2026-09-05 23:28:40 +0100

    01_oop

---
+++ OOP/ex01/S1E7.py
@@ -0,0 +1,73 @@
+from S1E9 import Character
+
+
+class Baratheon(Character):
+    """Representing the Baratheon family"""
+    def __init__(self, first_name: str, is_alive: bool = True) -> None:
+        """Initialize a Baratheon character"""
+        super().__init__(first_name, is_alive)
+        self.family_name = "Baratheon"
+        self.eyes = "brown"
+        self.hairs = "dark"
+
+    def die(self) -> None:
+        """Mark the character as dead"""
+        self.is_alive = False
+
+    def __str__(self) -> str:
+        """Return a string representation of the family character"""
+        return f"Vector: ('Baratheon', '{self.eyes}', '{self.hairs}')"
+
+    def __repr__(self) -> str:
+        """Return a string representation of the family character"""
+        return self.__str__()
+
+
+class Lannister(Character):
+    """Representing the Lannister family"""
+    def __init__(self, first_name: str, is_alive: bool = True) -> None:
+        """Initialize a Lannister character"""
+        super().__init__(first_name, is_alive)
+        self.family_name = "Lannister"
+        self.eyes = "blue"
+        self.hairs = "light"
+
+    def die(self) -> None:
+        """Mark the character as dead"""
+        self.is_alive = False
+
+    def __str__(self) -> str:
+        """Return a string representation of the family character"""
+        return f"Vector: ('Lannister', '{self.eyes}', '{self.hairs}')"
+
+    def __repr__(self) -> str:
+        """Return a string representation of the family character"""
+        return self.__str__()
+
+    @classmethod
+    def create_lannister(cls, first_name: str, is_alive: bool = True):
+        """Create a Lannister character with a default first name"""
+        return cls(first_name, is_alive)
+
+
+def main() -> None:
+    Robert = Baratheon("Robert")
+    print(Robert.__dict__)
+    print(Robert.__str__)
+    print(Robert.__repr__)
+    print(Robert.is_alive)
+    Robert.die()
+    print(Robert.is_alive)
+    print(Robert.__doc__)
+    print("---")
+    Cersei = Lannister("Cersei")
+    print(Cersei.__dict__)
+    print(Cersei.__str__)
+    print(Cersei.is_alive)
+    print("---")
+    Jaine = Lannister.create_lannister("Jaine", True)
+    print(f"Name : {Jaine.first_name, type(Jaine).__name__}, Alive : {Jaine.is_alive}")
+
+
+if __name__ == "__main__":
+    main()
